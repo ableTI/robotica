f6832123c23f394c3069bad6f8b19a896c705e51
Abel Tim <git@abeltim.com>
2025-06-12 10:28:13 +0200

    recenltly removed three commit becouse it included files that shouldn't be included.
    Did reset hard,
    localy unstaged commits, deleted files and recommited the good files.
    think this is all i did since,
    -changed extra PCB name to bave and _ instead of a space so i dont have to use ""
    -added button and led to teensy41 goaly overlay
    -started on PWM, not complete
    
    Abel Tim <git@abeltim.com>

--- code/main/teensy_goalie/src/main.c
+++ code/main/teensy_goalie/src/main.c
@@ -1,48 +1,17 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+// Settings
+#define PWM_PERIOD_NS 20000000
+#define PWM_PULSE_WIDTH 1400000
+static const int32_t sleep_time_ms = 100;
+static const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(DT_ALIAS(powerswitch), gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(redled), gpios);
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-int main(void)
-{
-    int ret;
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) {
-        return 0;
-    }
-
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return 0;
-    }
-
-    while (1) {
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return 0;
-        }
-
-        led_state = !led_state;
-        printf("LED state: %s\n", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
-    }
-    return 0;
-}
+#define LED0_NODE DT_ALIAS(redled)
+#define PWM_LED0 DT_ALIAS(en3a)
--- code/main/teensy_player/src/main.c
+++ code/main/teensy_player/src/main.c
@@ -7,8 +7,10 @@
 #include <zephyr/drivers/pwm.h>
 #include <string.h>
 
-/* size of stack area used by each thread */
-#define GEN_THRESD_STACKSIZE 1024
+/* Stack sizes (now only one size)*/
+/* To figure out how much is necessary: */
+/* https://zephyr-docs.listenai.com/guides/debug_tools/thread-analyzer.html */
+#define GENERAL_THREAD_STACKSIZE 1024
 
 /*the pinout configuration*/
 /*https://docs.zephyrproject.org/latest/boards/pjrc/teensy4/doc/index.html*/
@@ -18,23 +20,21 @@
 #define m1pwm1 DT_ALIAS(GPIO4_6)    /*pin 4*/
 #define m1pwm2 DT_ALIAS(GPIO4_5)    /*pin 3*/
 /*encoder pins*/
-#define m1encoA 32  /*pin 32*//*encoder number 1*/
-#define Nm1encoA INT_TO_POINTER(0)    /*gives encoder number N*/
-#define m1encoB 31  /*pin 31*//*encoder number 2*/
-#define Nm1encoB INT_TO_POINTER(1)    /*gives encoder number N*/
-
+#define m1encoA 32  /*pin 32   &&   encoder number 0*/
+#define Nm1encoA INT_TO_POINTER(0)
+#define m1encoB 31  /*pin 31   &&   encoder number 1*/
+#define Nm1encoB INT_TO_POINTER(1)
 
 /*set priorities*/
-#define ENC_ISR_PRIO 1 /* set encoder interrupt service routine*/
+#define ENC_ISR_PRIO 1
 
-
-/*variables*/
 /*encoders/PWM/PID*/
 #define NUM_ENCODERS 2     /*how many encoder pins there are*/
-volatile int encCount[NUM_ENCODERS] = {0};  /*sets all encoder counts to 0 changes of state*/
+volatile int encCount[NUM_ENCODERS] = {0};
 
 /*misc*/
-#define MY_IRQ_FLAGS 0       /*no IRQ flags*/
+#define NO_IRQ_FLAGS 0
+#define PWM_PERIOD_µS 1600   /*The lengt of an full PWM cycle in microseconds*/
 
 void ENCO_ISR(void *encN){
     /*adds one to the encoder where a change of state is detected*/
@@ -46,9 +46,9 @@ void ENCO_ISR(void *encN){
 }
 
 void ENC_ISR_installer(void){
-    /*sets up the interupts for the encoder at same priority, need to configure FIFO in case of simultaneous call*/
-    IRQ_CONNECT(m1encoA, ENC_ISR_PRIO, ENCO_ISR, '0', MY_IRQ_FLAGS);
-    IRQ_CONNECT(m1encoB, ENC_ISR_PRIO, ENCO_ISR, '1', MY_IRQ_FLAGS);
+    /*sets up the interrupts for the encoder at same priority, need to configure FIFO in case of simultaneous call*/
+    IRQ_CONNECT(m1encoA, ENC_ISR_PRIO, ENCO_ISR, '0', NO_IRQ_FLAGS);
+    IRQ_CONNECT(m1encoB, ENC_ISR_PRIO, ENCO_ISR, '1', NO_IRQ_FLAGS);
 }
 
 int main(void){}
